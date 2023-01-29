321b261829b08c0ee3ca4b4bb6f4069c69f150e2
Finja14 <123866495+Finja14@users.noreply.github.com>
2023-01-29 11:52:24 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,44 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . # . .
-        # . # # #
-        . # # # .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . # . # .
+                . . # . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        }
+    } else {
+        basic.clearScreen()
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                # . . . #
+                . # # # .
+                `)
+        } else {
+            basic.clearScreen()
+            if (input.buttonIsPressed(Button.B)) {
+                basic.showLeds(`
+                    # # . # #
+                    # # . # #
+                    . . . . .
+                    . . . . .
+                    . # # # .
+                    `)
+            } else {
+                basic.clearScreen()
+            }
+        }
     }
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . . . .
-        . . . . .
-        . # # # .
-        `)
 })
